e38c5edcbf70a1907189d9b18f310648dfdf28df
Fyamada1229 <yamadafutosi1229@gmail.com>
2022-12-28 08:20:23 +0900

    Main 追記

--- src/training/Main.java
+++ src/training/Main.java
@@ -1,9 +1,36 @@
 package training;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(training03(args[0]));
 	}
+	
+	public static String training03(String result) {
+		
+		//System.out.println(getComputer());
+		
+		return result;
+	}
+	
+	
+	public static String getComputer() {
+		// 乱数の準備
+		Random rand = new Random();
+		int changeRand = rand.nextInt(5);
+		
+		while (changeRand == 1 || changeRand == 3 || changeRand == 4) {
+			changeRand = rand.nextInt(5);
+			if(changeRand == 0 || changeRand == 2 || changeRand == 5) {
+				break;
+			}
+		}
+		Integer i = Integer.valueOf(changeRand);
+		String strChangeRand = i.toString();
+		// 0,2,5のどれかを、メソッドの結果として返す
+		return strChangeRand;
+		}
 
 }
