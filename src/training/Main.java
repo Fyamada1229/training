845d97d24b911978af164fb269fc425e5982e05d
Fyamada1229 <yamadafutosi1229@gmail.com>
2023-01-08 14:08:14 +0900

    ジャンケンのロジック修正

--- src/training/Main.java
+++ src/training/Main.java
@@ -5,64 +5,65 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args ) throws NumberFormatException, Exception {
+	public static void main(String[] args) throws NumberFormatException, Exception {
 		try {
 			// argsの値は文字列の0が入っている
-			System.out.println(training03(args[0]));
+			System.out.println(training03("0"));
 		} catch (NumberFormatException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
 			System.out.println("Exceptionが発生しました");
 		}
-		
+
 	}
-	
+
 	public static String training03(String result) {
 		// 数字のみ判定する
 		boolean intResult = false;
-	    if (result != null) {
-	        Pattern pattern = Pattern.compile("^[0-9]+$");
-	        intResult = pattern.matcher(result).matches();
-	        if (intResult == false) {
-	            throw new NumberFormatException("NumberFormatExceptionが発生しました、数字以外の文字が入っています");
-	        }
-	    }
-	    // 文字列以外が入っている場合の判定 「Comparable比較可能」
-	    Comparable<?> num = Integer.valueOf(1);
-	    if (result instanceof String == false && num instanceof Number) {
-	    	throw new NumberFormatException("NumberFormatExceptionが発生しました、整数が入っています");
-	    }
-	    
-	    int cpu = getComputer();
-	    
-	    // resultを整数にして比較する
-	    int changeNumResult = Integer.parseInt(result);	
-	    
-	    if (changeNumResult == 0) {
-	    	System.out.println("自分の手はグー");
-	    } else if (changeNumResult == 2) {
-	    	System.out.println("自分の手はチョキー");
-	    } else {
-	    	System.out.println("自分の手はパー");
-	    }
-	    
-	    if (cpu == 0) {
-	    	System.out.println("相手の手はグー");
-	    } else if (cpu == 2) {
-	    	System.out.println("相手の手はチョキー");
-	    } else {
-	    	System.out.println("相手の手はパー");
-	    }
-	
-	    // 条件 0 グー, 2チョキー, 5パー
-		if(changeNumResult == 0 && cpu == 2 || changeNumResult == 2 && cpu == 5 || changeNumResult == 5 && cpu == 0) {
-			result="勝ち";
-		} else if(changeNumResult == 0 && cpu == 0 || changeNumResult == 2 && cpu == 2 || changeNumResult == 5 && cpu == 5) {
-			result="あいこ";
-		} else {
-			result="まけ";
+		if (result != null) {
+			Pattern pattern = Pattern.compile("^[0-9]+$");
+			intResult = pattern.matcher(result).matches();
+			if (intResult == false) {
+				throw new NumberFormatException("NumberFormatExceptionが発生しました、数字以外の文字が入っています");
+			}
 		}
-		
+		// 文字列以外が入っている場合の判定 「Comparable比較可能」
+		Comparable<?> num = Integer.valueOf(1);
+		if (result instanceof String == false && num instanceof Number) {
+			throw new NumberFormatException("NumberFormatExceptionが発生しました、整数が入っています");
+		}
+
+		int cpu = getComputer();
+
+		// resultを整数にして比較する
+		int changeNumResult = Integer.parseInt(result);
+
+		if (changeNumResult == 0) {
+			System.out.println("自分の手はグー");
+		} else if (changeNumResult == 2) {
+			System.out.println("自分の手はチョキー");
+		} else {
+			System.out.println("自分の手はパー");
+		}
+
+		if (cpu == 0) {
+			System.out.println("相手の手はグー");
+		} else if (cpu == 2) {
+			System.out.println("相手の手はチョキー");
+		} else {
+			System.out.println("相手の手はパー");
+		}
+
+		// 条件 0 グー, 2チョキー, 5パー
+		if (changeNumResult == 0 && cpu == 2 || changeNumResult == 2 && cpu == 5 || changeNumResult == 5 && cpu == 0) {
+			result = "勝ち";
+		} else if (changeNumResult == 0 && cpu == 0 || changeNumResult == 2 && cpu == 2
+				|| changeNumResult == 5 && cpu == 5) {
+			result = "あいこ";
+		} else {
+			result = "まけ";
+		}
+
 		if (result == "あいこ") {
 			System.out.println("あいこです。あいこでしょ！");
 			try {
@@ -71,19 +72,52 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
+		while (result == "あいこ") {
+			int cpu2 = getComputer();
+
+			if (changeNumResult == 0) {
+				System.out.println("自分の手はグー");
+			} else if (changeNumResult == 2) {
+				System.out.println("自分の手はチョキー");
+			} else {
+				System.out.println("自分の手はパー");
+			}
+
+			if (cpu2 == 0) {
+				System.out.println("相手の手はグー");
+			} else if (cpu2 == 2) {
+				System.out.println("相手の手はチョキー");
+			} else {
+				System.out.println("相手の手はパー");
+			}
+
+			// 条件 0 グー, 2チョキー, 5パー
+			if (changeNumResult == 0 && cpu2 == 2 || changeNumResult == 2 && cpu2 == 5
+					|| changeNumResult == 5 && cpu == 0) {
+				result = "勝ち";
+				break;
+			} else if (changeNumResult == 0 && cpu2 == 0 || changeNumResult == 2 && cpu2 == 2
+					|| changeNumResult == 5 && cpu2 == 5) {
+				result = "あいこ";
+				break;
+			} else {
+				result = "まけ";
+			}
+
+		}
 
 		return result;
 	}
-	
+
 	public static int getComputer() {
 		// 乱数の準備
 		Random rand = new Random();
 		int changeRand = rand.nextInt(5);
-		
+
 		while (changeRand == 1 || changeRand == 3 || changeRand == 4) {
 			double secoundRand = Math.random() * 5 + 1;
-			changeRand = (int)secoundRand;
-			if(changeRand == 0 || changeRand == 2 || changeRand == 5) {
+			changeRand = (int) secoundRand;
+			if (changeRand == 0 || changeRand == 2 || changeRand == 5) {
 				break;
 			}
 		}
